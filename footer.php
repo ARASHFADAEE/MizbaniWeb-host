f8d4d3acb7fc5d78245e290132197dab75de8d20
ARASHFADAEE <mehrdadfadaee4@gmail.com>
2023-10-12 01:28:37 -0700

    show img in front page

---
+++ footer.php
@@ -0,0 +1,174 @@
+    <!--footer section start-->
+    <footer class="footer-section">
+        <!--footer top start-->
+        <div class="footer-top gradient-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="row footer-top-wrap">
+                            <div class="col-12">
+                                <div class="footer-nav-wrap text-white">
+                                    <h4 class="text-white">قدرت گرفته از</h4>
+                                    <ul class="list-inline security-icon-list">
+                                        <li class="list-inline-item"><img src="assets/img/mcafee-logo.png" width="110" alt="security" class="img-fluid" /></li>
+                                        <li class="list-inline-item"><img src="assets/img/norton-logo.png" width="110" alt="security" class="img-fluid" /></li>
+                                        <li class="list-inline-item"><img src="assets/img/accredited-logo.png" width="110" alt="security" class="img-fluid" /></li>
+                                        <li class="list-inline-item"><img src="assets/img/secured-logo.png" width="110" alt="security" class="img-fluid" /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="row footer-top-wrap">
+                            <div class="col-md-3 col-sm-6">
+                                <div class="footer-nav-wrap text-white">
+                                    <h4 class="text-white">محصولات</h4>
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">میزبانی وردپرس</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">میزبانی اشتراکی</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">سرور مجازی</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">میزبانی اختصاصی</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">جستجوی دامنه</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-6">
+                                <div class="footer-nav-wrap text-white">
+                                    <h4 class="text-white">شرکت</h4>
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">درباره ما</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">شرکا</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">وبلاگ</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">فرصت های شغلی</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">شبکه های اجتماعی</a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-6">
+                                <div class="footer-nav-wrap text-white">
+                                    <h4 class="text-white">قوانین</h4>
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">اطلاعات حقوقی</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">حفظ حریم خصوصی</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">گزارش سو استفاده</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">شرایط استفاده از خدمات</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">بررسی WHOIS</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-6">
+                                <div class="footer-nav-wrap text-white">
+                                    <h4 class="text-white">پشتیبانی</h4>
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">ارتباط با ما</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">آموزش</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">انجمن</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">وضعیت سیستم</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">تیم پشتیبانی</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--footer top end-->
+
+        <!--footer copyright start-->
+        <div class="footer-bottom gray-light-bg py-3">
+            <div class="container">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-md-5 col-lg-5">
+                        <p class="copyright-text pb-0 mb-0">کپی رایت <span class="date"></span> © همه حقوق محفوظ است <a href="https://rtl-theme.com/author/chaveamin" target="_blank">دیزاینستا</a>
+                        </p>
+                    </div>
+                    <div class="col-md-7 col-lg-6">
+                        <div class="payment-method text-right">
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <img src="assets/img/payment-amex.svg" alt="amex" width="50" />
+                                </li>
+                                <li class="list-inline-item">
+                                    <img src="assets/img/payment-visa.svg" alt="visa" width="50" />
+                                </li>
+                                <li class="list-inline-item">
+                                    <img src="assets/img/payment-mastercard.svg" alt="mastercard" width="50" />
+                                </li>
+                                <li class="list-inline-item">
+                                    <img src="assets/img/payment-paypal.svg" alt="paypal" width="50" />
+                                </li>
+                                <li class="list-inline-item">
+                                    <img src="assets/img/payment-bitcoin.svg" alt="bitcoin" width="50" />
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--footer copyright end-->
+    </footer>
+    <!--footer section end-->
+    <!--bottom to top button start-->
+    <button class="scroll-top scroll-to-target" data-target="html">
+        <span class="ti-rocket"></span>
+    </button>
+    <!--bottom to top button end-->
+    <!--build:js-->
+    <script src="assets/js/vendors/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/vendors/popper.min.js"></script>
+    <script src="assets/js/vendors/rtl.bootstrap.min.js"></script>
+    <script src="assets/js/vendors/bootstrap-slider.min.js"></script>
+    <script src="assets/js/vendors/jquery.countdown.min.js"></script>
+    <script src="assets/js/vendors/jquery.easing.min.js"></script>
+    <script src="assets/js/vendors/owl.carousel.min.js"></script>
+    <script src="assets/js/vendors/validator.min.js"></script>
+    <script src="assets/js/vendors/jquery.waypoints.min.js"></script>
+    <script src="assets/js/vendors/jquery.rcounterup.js"></script>
+    <script src="assets/js/vendors/magnific-popup.min.js"></script>
+    <script src="assets/js/vendors/hs.megamenu.js"></script>
+    <script src="assets/js/app.js"></script>
+    <!--endbuild-->
