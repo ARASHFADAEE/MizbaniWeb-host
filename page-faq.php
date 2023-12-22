29626cb7c5b8122f3d6ed32ff73edb1ff40242d3
ARASHFADAEE <mehrdadfadaee4@gmail.com>
2023-12-22 05:39:16 -0800

    add faq page

---
+++ page-faq.php
@@ -0,0 +1,264 @@
+<?php get_header()?>
+    <div class="main">
+
+        <!--page header section start-->
+        <section class="page-header-section ptb-100 gradient-bg">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-7 col-lg-6">
+                        <div class="page-header-content text-white">
+                            <h1 class="text-white mb-2">سوالات متداول</h1>
+                            <p class="lead">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--page header section end-->
+
+        <!--breadcrumb bar start-->
+        <div class="breadcrumb-bar py-3 gray-light-bg border-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="custom-breadcrumb">
+                            <ol class="breadcrumb d-inline-block bg-transparent list-inline py-0 pl-0">
+                                <li class="list-inline-item breadcrumb-item"><a href="#">خانه</a></li>
+                                <li class="list-inline-item breadcrumb-item"><a href="#">صفحات جانبی</a></li>
+                                <li class="list-inline-item breadcrumb-item active">سوالات متداول</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--breadcrumb bar end-->
+
+        <!--faq section start-->
+        <div id="faq" class="ptb-100 ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9 col-lg-8">
+                        <div class="section-heading mb-5">
+                            <h2>سوالات متداول</h2>
+                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است <a href="#contact" class="page-scroll">ارتباط با ما</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-lg-12">
+                        <div id="accordion-one" class="accordion accordion-faq">
+                            <div class="card mb-0">
+                                <a class="card-header collapsed" data-toggle="collapse" href="#collapseOne">
+                                    <h6 class="mb-0 d-inline-block">میزبانی وب چیست؟
+                                    </h6>
+                                </a>
+                                <div id="collapseOne" class="collapse" data-parent="#accordion-one">
+                                    <div class="card-body">
+                                        <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان را به کارفرما نمایش دهند، استفاده از این متن تستی می تواند سرعت پیشرفت پروژه.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mb-0">
+                                <a class="card-header collapsed" data-toggle="collapse" href="#collapseTwo">
+                                    <h6 class="mb-0 d-inline-block">تفاوت محصولات ما با DreamHost چیست؟</h6>
+                                </a>
+                                <div id="collapseTwo" class="collapse" data-parent="#accordion-one">
+                                    <div class="card-body">
+                                        <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان را به کارفرما نمایش دهند، استفاده از این متن تستی می تواند سرعت پیشرفت پروژه.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mb-0">
+                                <a class="card-header collapsed" data-toggle="collapse" href="#collapseThree">
+                                    <h6 class="mb-0 d-inline-block">چگونه یک دامین بخرم؟
+                                    </h6>
+                                </a>
+                                <div id="collapseThree" class="collapse" data-parent="#accordion-one">
+                                    <div class="card-body">
+                                        <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان را به کارفرما نمایش دهند، استفاده از این متن تستی می تواند سرعت پیشرفت پروژه.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mb-0">
+                                <a class="card-header collapsed" data-toggle="collapse" href="#collapseFour">
+                                    <h6 class="mb-0 d-inline-block">آیا می توانید به من کمک کنید تا درباره وردپرس بیشتر بدانم؟
+                                    </h6>
+                                </a>
+                                <div id="collapseFour" class="collapse" data-parent="#accordion-one">
+                                    <div class="card-body">
+                                        <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان را به کارفرما نمایش دهند، استفاده از این متن تستی می تواند سرعت پیشرفت پروژه.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!--faq section end-->
+
+        <!--faq new style start-->
+        <section class="ptb-100 gray-light-bg">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-9 col-lg-8">
+                        <div class="section-heading mb-3 text-center">
+                            <h2>سوالات متداول</h2>
+                            <p class="lead">
+                                این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="single-faq pt-4">
+                            <h5>مراحل پرداخت چگونه است؟</h5>
+                            <p>از پروژه و طرح خودشان را به کارفرما نمایش دهند، استفاده از این متن تستی می تواند سرعت پیشرفت پروژه را افزایش دهد، و طراحان به جای تایپ.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="single-faq pt-4">
+                            <h5>آیا پلن سالانه دارید؟</h5>
+                            <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان را به کارفرما نمایش دهند، استفاده از این متن تستی می تواند سرعت پیشرفت پروژه را افزایش دهد. </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="single-faq pt-4">
+                            <h5>آیا برای چند کالا تخفیف می دهید؟</h5>
+                            <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان را به کارفرما نمایش دهند، استفاده از این متن تستی می تواند سرعت پیشرفت پروژه را افزایش دهد</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="single-faq pt-4">
+                            <h5>آیا مالیات بر ارزش افزوده در قیمت طرح لحاظ می شود؟</h5>
+                            <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان. </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="single-faq pt-4">
+                            <h5>آیا برای برخی از ویژگی ها هزینه بیشتری خواهم پرداخت؟</h5>
+                            <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="single-faq pt-4">
+                            <h5>چرا هیچ محدودیتی در تعداد پیام ها وجود ندارد؟</h5>
+                            <p>این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند نمونه تکمیل شده از پروژه و طرح خودشان.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--faq new style end-->
+
+        <!--call to action section start-->
+        <section class="call-to-action ptb-100 gradient-overlay " style="background: url('assets/img/hero-bg-4.jpg')no-repeat center center / cover">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-9 col-lg-8">
+                        <div class="cta-new-wrap text-white text-center">
+                            <h2 class="text-white">30 روز بازگشت وجه</h2>
+                            <p class="lead">این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی بتوانند.</p>
+                            <div class="action-btns mt-4">
+                                <a href="#" class="btn solid-white-btn animated-btn mr-lg-3">همین الان شروع کنید</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--call to action section end-->
+
+        <!--submit request form start-->
+        <section class="submit-request-form ptb-100">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-9 col-lg-8">
+                        <div class="section-heading text-center mb-5">
+                            <h2>ارسال درخواست</h2>
+                            <p class="lead"> این یک نوشته آزمایشی است که به طراحان و برنامه نویسان کمک میکند تا این عزیزان با بهره گیری از این نوشته تستی و آزمایشی.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-lg-12">
+                        <form class="submit-request-form">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="select1">سلام ما در چه زمینه ای می توانیم به شما کمک کنیم؟</label>
+                                        <select id="select1" class="crumina--select input--squared select2-hidden-accessible form-control" data-minimum-results-for-search="Infinity" tabindex="-1" aria-hidden="true">
+                                            <option data-display="Domain Registrations">ثبت دامنه</option>
+                                            <option value="1">شروع میزبانی</option>
+                                            <option value="2">میزبانی</option>
+                                            <option value="3">ثبت دامنه</option>
+                                            <option value="4">عمومی</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="name">نام <span class="required">*</span></label>
+                                        <input id="name" class="form-control" type="text" placeholder="نام">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="url">وبسایت</label>
+                                        <input id="url" class="form-control" type="text" placeholder="https://">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="select2">توضیحات بیشتر <span class="required">*</span></label>
+                                        <select id="select2" class="form-control" data-minimum-results-for-search="Infinity" tabindex="-1" aria-hidden="true">
+                                            <option data-display="Domain Registrations">اگر ثبت دامنه من منقضی شود چه می شود؟
+                                            </option>
+                                            <option value="1">شروع میزبانی</option>
+                                            <option value="2">میزبانی</option>
+                                            <option value="3">ثبت دامنه</option>
+                                            <option value="4">عمومی</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="submitEmail">آدرس ایمیل <span class="required">*</span></label>
+                                        <input id="submitEmail" class="form-control" type="email" placeholder="ایمیل">
+                                    </div>
+                                    <div class="form-group input-file-wrap">
+                                        <label for="upfile">پیوست ها</label>
+                                        <span class="upfile-btn">آپلود فایل</span>
+                                        <input id="upfile" type="file" class="form-control" />
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="descr">توضیحات مفصل ارائه دهید <span
+                                            class="required">*</span></label>
+                                        <textarea id="descr" class="form-control" placeholder="" rows="4"></textarea>
+                                        <p class="small mt-2">لطفا جزئیات درخواست خود را وارد کنید. یکی از کارکنان پشتیبانی ما در اسرع وقت پاسخ خواهد داد.</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <button type="button" class="btn primary-solid-btn">ارسال تیکت</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--submit request form end-->
+
+
+    </div>
+
+    <!--footer section start-->
+<?php get_footer()?>
+</body>
+
+</html>
